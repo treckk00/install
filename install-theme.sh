59f2401978e9eef59889ba2ac57e0a755a5533d7
treckk00 <85478471+treckk00@users.noreply.github.com>
2021-07-26 04:14:20 -0400

    Add files via upload

--- install-theme.sh
+++ install-theme.sh
@@ -10,24 +10,27 @@ if [ ! -f "artisan" ]; then
     cd /var/www/pterodactyl
     else
 
-	echo "Downloading the Theme"
-	mkdir -p tempdown && cd tempdown && git clone https://github.com/treckk00/pageDesign.git .
-	cp -r app ..
-	cp -r config ..
-	cp -r database ..
-	cp -r public ..
-	cp -r resources ..
-	cp -r routes ..
+    echo "Your Artisan File has been found!"
+    sleep 2
 
-	echo "Files have been copied over!"
-	sleep 2
+    echo "Downloading the Theme"
+    mkdir -p tempdown && cd tempdown && git clone https://github.com/treckk00/pageDesign.git .
+    cp -r app ..
+    cp -r config ..
+    cp -r database ..
+    cp -r public ..
+    cp -r resources ..
+    cp -r routes ..
+    
 
-	echo "Removing the temp folders created in the copy process"
+    echo "Files have been copied over!"
+    sleep 2
 
-	cd .. && rm -rf tempdown
-	sleep 2
-	php artisan migrate --path=/database/migrations/2021_05_30_141248_create_unix_settings_table.php
-	sleep 2
+    echo "Removing the temp folders created in the copy process"
 
-	echo "Complete! Have a good day"
+    cd .. && rm -rf tempdown
+    php artisan migrate --path=/database/migrations/2021_05_30_141248_create_unix_settings_table.php
+
+    echo "Complete! Have a good day and dont forget to refresh your browser cache! (CTRL + F5)"
+    echo "-Will"
 fi
