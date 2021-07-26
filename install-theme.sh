53c4afaee0a84eaccd99dfd05b8022d347e81ba9
treckk00 <85478471+treckk00@users.noreply.github.com>
2021-07-26 04:11:09 -0400

    Add files via upload

--- install-theme.sh
+++ install-theme.sh
@@ -1,25 +1,33 @@
 #!/bin/sh
 
-cd /var/www/pterodactyl
-sleep 2
+if [ ! -f "artisan" ]; then
+    echo "Could not find the Artisan file, Moving to Default Location."
+    cd /var/www/pterodactyl
+fi
 
-echo "Downloading the Theme"
-mkdir -p tempdown && cd tempdown && git clone https://github.com/treckk00/pageDesign.git .
-cp -r app ..
-cp -r config ..
-cp -r database ..
-cp -r public ..
-cp -r resources ..
-cp -r routes ..
+if [ ! -f "artisan" ]; then
+    echo "We tried to find your Artisan file but we couldnt, Please move to the directory you installed the Panel and re-run this script. Have a Good Day!"
+    cd /var/www/pterodactyl
+    else
 
-echo "Files have been copied over!"
-sleep 2
+	echo "Downloading the Theme"
+	mkdir -p tempdown && cd tempdown && git clone https://github.com/treckk00/pageDesign.git .
+	cp -r app ..
+	cp -r config ..
+	cp -r database ..
+	cp -r public ..
+	cp -r resources ..
+	cp -r routes ..
 
-echo "Removing the temp folders created in the copy process"
+	echo "Files have been copied over!"
+	sleep 2
 
-cd .. && rm -rf tempdown
-sleep 2
-php artisan migrate --path=/database/migrations/2021_05_30_141248_create_unix_settings_table.php
-sleep 2
+	echo "Removing the temp folders created in the copy process"
 
-echo "Complete! Have a good day"
+	cd .. && rm -rf tempdown
+	sleep 2
+	php artisan migrate --path=/database/migrations/2021_05_30_141248_create_unix_settings_table.php
+	sleep 2
+
+	echo "Complete! Have a good day"
+fi
