fff23e3f4fc1552d5496f674f37749d1dc55b227
treckk00 <85478471+treckk00@users.noreply.github.com>
2021-07-26 03:54:11 -0400

    Add files via upload

--- config-mysql.sh
+++ config-mysql.sh
@@ -1,17 +1,28 @@
 #!/bin/sh
+
 echo "* Create MySQL user."
 mysql -u root -p '' "create user 'user'@'%' identified by 'Password1';"
+sleep 2
 mysql -u root -p '' "grant all privileges on *.* to 'user'@'%' with grant option;"
+sleep 2
 echo "* Flushing privileges.."
+sleep 2
 mysql -u root -p '' "FLUSH PRIVILEGES;"
+sleep 2
 
 echo "* Create MySQL user."
+sleep 2
 mysql -u root -p '' "create user 'server'@'%' identified by 'Password1';"
+sleep 2
 mysql -u root -p '' "CREATE DATABASE server;"
+sleep 2
 mysql -u root -p '' "grant all privileges on server.* to 'server'@'%';"
+sleep 2
 
 echo "* Flushing privileges.."
+sleep 2
 mysql -u root -p '' "FLUSH PRIVILEGES;"
+sleep 2
 
 echo "* MySQL database created & configured!"
 exit
--- config-port.sh
+++ config-port.sh
@@ -1,12 +1,22 @@
 #!/bin/sh
+
 echo "* Puertos Configurados."
 apt install ufw
+sleep 2
 ufw allow 22
+sleep 2
 ufw allow 80
+sleep 2
 ufw allow 8080
+sleep 2
 ufw allow 443
+sleep 2
 ufw allow 30120
+sleep 2
 ufw allow 40120
+sleep 2
 ufw allow 3306
+sleep 2
 ufw allow 2022
+sleep 2
 ufw enable
